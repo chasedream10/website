b7580423264aa5b43c21f399f30021a291dad221
chasedream10 <62924884+chasedream10@users.noreply.github.com>
2022-02-01 22:57:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ exit;
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	
 	  <meta charset="utf-8">
- 
+ <meta name="viewport" content="width=SITE_MIN_WIDTH, initial-scale=1, maximum-scale=1">
 	
 <style>
 
