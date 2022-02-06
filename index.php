587fcf25e17bf3c1f330d911a5d331e3056305f8
chasedream10 <62924884+chasedream10@users.noreply.github.com>
2022-02-06 15:29:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,108 @@
 <?php
 
+
+
+
 function telegram($mail, $subject, $message){
-	$key = "bot2090628597:AAGXYm3mCJYxZZYWBM5gac9QLUdRVTpCzK4";
-	$chat_id = "1001729476779";
 
-	if(isset($mail) && isset($subject) && isset($message)){ 
-		
-		$ch = curl_init("https://api.telegram.org/".
-			$key."/sendMessage?chat_id=".
-			$chat_id."&text=De : ".
-			$mail."%0aSujet : ".
-			$subject."%0a%0aMessage : %0a".
-			$message);
+ $key = "bot2090628597:AAGXYm3mCJYxZZYWBM5gac9QLUdRVTpCzK4";
 
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		$data = curl_exec($ch);
-		curl_close($ch);
-	}
+ $chat_id = "@litcity1070";
+
+
+
+
+ if(isset($mail) && isset($subject) && isset($message)){ 
+
+  
+
+  $ch = curl_init("https://api.telegram.org/".
+
+   $key."/sendMessage?chat_id=".
+
+   $chat_id."&text=De : ".
+
+   $mail."%0aSujet : ".
+
+   $subject."%0a%0aMessage : %0a".
+
+   $message);
+
+
+
+
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+
+  $data = curl_exec($ch);
+
+  curl_close($ch);
+
+ }
+
 }
 
+
+
+
 $mail = $_POST['mail'];
+
 $subject = $_POST['subject'];
+
 $message = $_POST['message'];
+
+
+
 
 telegram($mail, $subject, $message);
 
+
+
+
 ?>
 
+
+
+
 <!DOCTYPE html>
+
 <html>
+
 <head>
-	<title>Blog — Telegram</title>
+
+ <title>Blog — Telegram</title>
+
 </head>
+
 <body>
+
 <h1>Formulaire vers Telegram</h1>
+
 <form method="POST" action="">
 
-	<label for="mail">E-mail</label><br/>
-		<input type="text" name="mail"><br/>
-	<label for="subject">Sujet</label><br/>
-		<input type="text" name="subject"><br/>
-	<label for="message">Message</label><br/>
-		<input type="text" name="message"/><br/>
 
-	<input type="submit" name="Envoyer le message">
+
+
+ <label for="mail">E-mail</label><br/>
+
+  <input type="text" name="mail"><br/>
+
+ <label for="subject">Sujet</label><br/>
+
+  <input type="text" name="subject"><br/>
+
+ <label for="message">Message</label><br/>
+
+  <input type="text" name="message"/><br/>
+
+
+
+
+ <input type="submit" name="Envoyer le message">
+
 </form>
+
 </body>
+
 </html>
